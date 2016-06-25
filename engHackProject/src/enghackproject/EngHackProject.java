0f0eabd40334a87009e77d3bab020c0bb7958fd1
shireen taha <shireen taha>
2016-06-25 15:11:21 -0400

    'all'

--- engHackProject/src/enghackproject/EngHackProject.java
+++ engHackProject/src/enghackproject/EngHackProject.java
@@ -5,6 +5,9 @@
  */
 package enghackproject;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 /**
  *
  * @author Rafael
@@ -15,7 +18,11 @@ public class EngHackProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+         JFrame frame = new JFrame("Frame");
+        frame.setSize(300,400);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
     
 }
