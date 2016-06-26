7e99890402e862c3bae581fe45e20b28322f59b5
shireen taha <shireen taha>
2016-06-25 21:47:00 -0400

    'what'

--- engHackProject/src/enghackproject/Contents.java
+++ engHackProject/src/enghackproject/Contents.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -20,27 +21,22 @@ import javax.swing.Timer;
  * @author Rafael
  */
 public class Contents extends JPanel implements ActionListener {
-    
+    private Stone ston;
     private Bird bird;
     private Timer timer;
 
     private Image stone;
     private int posX = 0, posY = 0;
     private int speedX = 1, speedY = 1;
-    private ImageIcon left_to_right = new ImageIcon(this.getClass().getResource("stone1.png"));
-    private ImageIcon right_to_left = new ImageIcon(this.getClass().getResource("stone1.png"));
+    
     
     public Contents() {
         super.setDoubleBuffered(true);
-        addKeyListener(new KeyInput(this));
-        setFocusable(true);
-        requestFocusInWindow();
-        
-        bird = new Bird(50,0);
-        
-        
+        ston = new Stone(50,450);
+        bird = new Bird(0,50);
         timer = new Timer(10,this);
         timer.start();
+        
     }
     
     @Override
@@ -48,34 +44,24 @@ public class Contents extends JPanel implements ActionListener {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
-        if(speedX > 0) {
-            stone = left_to_right.getImage();
-        }else {
-            stone = right_to_left.getImage();
-        }
         
-        g2d.drawImage(stone, posX,posY, this);
+        
+        //g2d.drawImage(stone, posX,posY, this);
         bird.draw(this, g2d);
+        ston.draw(this, g2d);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         bird.move();
-
-        posX += speedX;
-        posY=450;
-        if(posX == 0) {
-            speedX = 1;
-        }
-        else if(posX == 350) {
-            speedX = -1;
-        }
-
+        ston.move();
         repaint();
     }
 
-    public void keyPressed(KeyEvent e) {
+        public void keyPressed(KeyEvent e) {
+            
         if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+            System.out.println("SPACE PRESSED LOL");
             bird.die();
         }
     }
---
+++ engHackProject/src/enghackproject/Stone.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enghackproject;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ *
+ * @author shireen
+ */
+public class Stone {
+    
+    //private boolean alive;
+    private int x, y, speed_x, speed_y, repeat;
+    boolean readyToFire, shot = false;
+    private Image stone;
+    private ImageIcon left_to_right = new ImageIcon(this.getClass().getResource("stone1.png"));
+    private ImageIcon right_to_left = new ImageIcon(this.getClass().getResource("stone1.png"));
+    
+    Rectangle bullet;
+    
+    public Stone(int x, int y){
+        repeat = 0;
+        this.x = x;
+        this.y = y;
+        speed_x = 1;
+        speed_y = 0;
+    }
+    
+    
+    public void draw(JPanel jpanel, Graphics2D g2d) {
+        g2d.drawImage(stone, x,y, jpanel);        
+    }
+    
+    public void move(){
+        x += speed_x;
+        y += speed_y;
+        limits();
+      
+    }
+    
+    private void limits(){
+        
+        if(x == 0) {
+            speed_x = 1;
+        }
+        else if(x == 350) {
+            speed_x = -1;
+        }
+
+        if(y == 0) {
+            speed_y = 1;
+        }
+        else if(y == 0) {
+            speed_y = -1;
+        }
+        if(x > 0) {
+            stone = left_to_right.getImage();
+        }else {
+            stone = right_to_left.getImage();
+        }
+    }
+    
+    public void keyPressed(KeyEvent e){
+        
+    }
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * @return the speed_x
+     */
+    public int getSpeed_x() {
+        return speed_x;
+    }
+
+    /**
+     * @param speed_x the speed_x to set
+     */
+    public void setSpeed_x(int speed_x) {
+        this.speed_x = speed_x;
+    }
+
+    /**
+     * @return the speed_y
+     */
+    public int getSpeed_y() {
+        return speed_y;
+    }
+
+    /**
+     * @param speed_y the speed_y to set
+     */
+    public void setSpeed_y(int speed_y) {
+        this.speed_y = speed_y;
+    }
+
+    /**
+     * @return the image
+     */
+    public Image getImage() {
+        return stone;
+    }
+
+    /**
+     * @param image the image to set
+     */
+    public void setImage(Image image) {
+        this.stone = image;
+    }
+    
+}
